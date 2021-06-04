8556a6313480409e1c85cea6496605e4411fe5ae
Greg <lewin.gc@gmail.com>
2021-06-04 14:43:48 -0400

    minor deets

--- 32u4-uart-comm/src/main.cpp
+++ 32u4-uart-comm/src/main.cpp
@@ -11,15 +11,25 @@ void loop()
 {
   while (Serial1.available()) 
   {
-    Serial1.print('\n');
-    char a = Serial1.read();
+    //Serial1.print('\n');
+    int a = Serial1.read();
 
-    Serial1.print(a);
-    Serial.print(a);
+    //Serial1.print(a);
+    Serial.println(a, HEX);
   }
-  
+
   while (Serial.available()) 
   {
     Serial1.print(char(Serial.read()));
+  }
+
+  static uint32_t lastPub = 0;
+  if(millis() - lastPub >= 500 ) 
+  {
+    lastPub = millis();
+    char message[20];
+    sprintf(message, "%li,%i\n", millis()%1000, analogRead(A0));
+    //Serial1.print(message);
+    //publishSerialData(message);
   }
 }
--- esp-uart-comm/src/main.cpp
+++ esp-uart-comm/src/main.cpp
@@ -1,16 +1,13 @@
 #include <Arduino.h>
 
-#define RXD2 16
-#define TXD2 17
+//#define RXD2 16
+//#define TXD2 17
 
 void setup() 
 {
   Serial.begin(115200);
 
-  Serial2.begin(9600, SERIAL_8N1, RXD2, TXD2);
-
-  Serial.println("Serial Txd is on pin: "+String(TX));
-  Serial.println("Serial Rxd is on pin: "+String(RX));
+  Serial2.begin(9600);//, SERIAL_8N1, RXD2, TXD2);
 }
 
 void loop() 
