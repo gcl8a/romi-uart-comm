aebab6ee907d771a8e47b5c9531f6cc39a12ab58
Greg <lewin.gc@gmail.com>
2021-08-13 20:27:35 -0400

    simplified

--- 32u4-uart-comm/src/main.cpp
+++ 32u4-uart-comm/src/main.cpp
@@ -3,33 +3,19 @@
 void setup() 
 {
   Serial.begin(115200);
-
-  Serial1.begin(9600);
+  Serial1.begin(115200);
 }
 
 void loop() 
 {
   while (Serial1.available()) 
   {
-    //Serial1.print('\n');
-    int a = Serial1.read();
-
-    //Serial1.print(a);
-    Serial.println(a, HEX);
+    char a = Serial1.read();
+    Serial.print(a);
   }
 
   while (Serial.available()) 
   {
     Serial1.print(char(Serial.read()));
-  }
-
-  static uint32_t lastPub = 0;
-  if(millis() - lastPub >= 500 ) 
-  {
-    lastPub = millis();
-    char message[20];
-    sprintf(message, "%li,%i\n", millis()%1000, analogRead(A0));
-    //Serial1.print(message);
-    //publishSerialData(message);
   }
 }
